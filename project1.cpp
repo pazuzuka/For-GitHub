517048329d38eb1b51c378414438ce2fba78d1a7
G0DFREE <grischenko.denis.ua@gmail.com>
2024-03-18 11:28:36 +0100

    Big Update Add Move Add Player Add Maze

--- project1.cpp
+++ project1.cpp
@@ -1,7 +1,80 @@
-#include<iostream>
+﻿#include<iostream>
 using namespace std;
 
-void main()
+void generateLab(int, int);
+void player(int&, int&, int, int, char);
+
+int main()
 {
-	cout << "Test Git" << endl;
+	char gamer = '@';
+	int playerRows = 1, playerColumns = 1;
+	int rows = 25, columns = 50;
+
+	generateLab(rows, columns);
+
+	while (true)
+	{
+		player(playerRows, playerColumns, rows, columns, gamer);
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (i == playerRows && j == playerColumns) {
+					cout << gamer;
+				}
+				else if (i == 0 || i == rows - 1 || j == 0 || j == columns - 1) {
+					cout << "#";
+				}
+				else {
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
+
+void generateLab(int rows, int columns)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			if (i == 0 || i == rows - 1 || j == 0 || j == columns - 1)
+			{
+				cout << "#";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+}
+
+void player(int& playerRows, int& playerColumns, int rows, int columns, char gamer)
+{
+	char moving;
+	cout << "Enter a move(W - Up, A - left, S - down, D - right): ";
+	cin >> moving;
+
+	switch (tolower(moving)) {
+	case 'w':
+		if (playerRows > 1) playerRows--;
+		break;
+	case 'a':
+		if (playerColumns > 1) playerColumns--;
+		break;
+	case 's':
+		if (playerRows < rows - 2) playerRows++;
+		break;
+	case 'd':
+		if (playerColumns < columns - 2) playerColumns++;
+		break;
+	default:
+		cout << "Invalid move! Please try again." << endl;
+		break;
+	}
 }
